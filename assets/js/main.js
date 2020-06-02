d39a6c037f1c78c0fafcc4f1164d6629bbc3067e
nnnlog <sorisem4106@naver.com>
2020-06-03 08:11:23 +0900

    코드 정렬, 메뉴 클릭 관련 오류 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,14 @@
 (() => {
-    const clickMenu = i => () => {
-            for (let e of document.querySelectorAll(".bottomNav > ul > li")) {
-                e.classList.remove('selectedMenu');
-            }
-            document.querySelector(`.bottomNav > ul > li:nth-child(${i})`).classList.add("selectedMenu")
-    };
-
-    let selectedMenu = 1;
-    window.addEventListener('DOMContentLoaded', () => {
-        let i = 0;
-        for (let e of document.querySelectorAll(".bottomNav > ul > li")) {
-            console.log(e)
-            e.onclick = clickMenu(++i);
-        }
-    });
+	let selectedMenu = 2; // Home btn
+	window.addEventListener('DOMContentLoaded', () => {
+		let i = 0;
+		for (let e of document.querySelectorAll(".bottomNav > ul > li")) {
+			let menu = ++i;
+			e.onclick = () => {
+				document.querySelector(`.bottomNav > ul > li:nth-child(${selectedMenu})`).classList.remove("selectedMenu");
+				document.querySelector(`.bottomNav > ul > li:nth-child(${menu})`).classList.add("selectedMenu");
+                selectedMenu = menu;
+			};
+		}
+	});
 })();
