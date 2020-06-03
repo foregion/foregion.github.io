64953fee6c7017ac16606e5b8e58084eb0cbf8bb
nnnlog <sorisem4106@naver.com>
2020-06-03 13:36:38 +0900

    카카오 지도 API 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 (() => {
+	let options = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+		draggable: true,
+		scrollwheel: true
+	};
+
 	let selectedMenu = 2; // Home btn
 	window.addEventListener('DOMContentLoaded', () => {
 		let i = 0;
@@ -7,8 +14,10 @@
 			e.onclick = () => {
 				document.querySelector(`.bottomNav > ul > li:nth-child(${selectedMenu})`).classList.remove("selectedMenu");
 				document.querySelector(`.bottomNav > ul > li:nth-child(${menu})`).classList.add("selectedMenu");
-                selectedMenu = menu;
+				selectedMenu = menu;
 			};
 		}
+
+		window.map = new kakao.maps.Map(document.querySelector("#map"), options);
 	});
 })();
