0aa6e45f2a2e1d89536d7929673def56345aea75
nnnlog <sorisem4106@naver.com>
2020-06-04 13:42:19 +0900

    스크롤 기능 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 (() => {
+	const calcHeight = element => {
+		return element.scrollHeight + parseInt(getComputedStyle(element).marginTop) + parseInt(getComputedStyle(element).marginBottom);
+	}
+
 	let options = {
 		center: new kakao.maps.LatLng(37.5536, 126.9696),
 		level: 3,
@@ -11,9 +15,9 @@
 	}, () => {
 	});
 
-	let selectedMenu = 2; // Home btn
 	window.addEventListener('DOMContentLoaded', () => {
 		let i = 0;
+		let selectedMenu = 2; // Home btn
 		for (let e of document.querySelectorAll(".bottomNav > ul > li")) {
 			let menu = ++i;
 			e.onclick = () => {
@@ -24,5 +28,45 @@
 		}
 
 		window.map = new kakao.maps.Map(document.querySelector("#map"), options);
+
+		const open = async () => {
+			let target = window.innerHeight
+				- calcHeight(document.querySelector("header > .topNav"))
+				- calcHeight(document.querySelector(".slider"))
+				- calcHeight(document.querySelector(".search-box"))
+				- calcHeight(document.querySelector(".bottomNav")) + 15;
+			let origin = document.querySelector(".scroll").scrollHeight;
+
+			for (let i = origin; i <= target; i++) {
+				if (i % 20 === 0) await new Promise(r => setTimeout(() => r(), 1));
+				document.querySelector(".scroll").style.height = `${i}px`;
+			}
+		}
+
+		let expand = false;
+		let min_height = document.querySelector(".scroll").scrollHeight;
+		document.querySelector(".slider").addEventListener('click', async function () {
+			if (!expand) await open();
+			else for (let i = document.querySelector(".scroll").scrollHeight; i >= min_height; i--) {
+				if (i % 20 === 0) await new Promise(r => setTimeout(() => r(), 1));
+				document.querySelector(".scroll").style.height = `${i}px`;
+			}
+			console.log(expand)
+
+			expand = !expand;
+		});
+
+		window.addEventListener('resize', () => {
+			if (expand) open();
+		});
+
+		var customOverlay = new kakao.maps.CustomOverlay({
+			position: new kakao.maps.LatLng(35.2038, 129.0859),
+			content: `<div><h1>동래시장</h1><div>동래시장</div></div>`,
+			xAnchor: 0.3,
+			yAnchor: 0.91
+		});
+
+		customOverlay.setMap(map);
 	});
 })();
